b8600d794b2b4adf86e2a7c99a1d3a2418466213
Leandro Kenzo Takara <62572529+devswordfish@users.noreply.github.com>
2023-05-11 11:10:51 -0300

    update Main.java

--- SegundoBimestre/Loja/Main.java
+++ SegundoBimestre/Loja/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Loja loja = new Loja("Capitão Salgados");
 
+        Cliente c1 = new Cliente("C1", "123.456.789-00", "12345-6789");
+
+        Pedido p1 = new Pedido(c1, "Batata Fritas", 12.99, 0);
+
+        loja.addCliente(c1);
+        loja.addPedido(p1);
+
+        loja.mostrarClientes();
     }
 }
